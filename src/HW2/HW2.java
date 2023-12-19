7386c30c071ef256d935ab7e32feec47cfd0f8b2
unknown <vitalii.programm@gmail.com>
2023-12-19 23:37:37 +0700

    hw2.1

--- src/HW2/HW2.java
+++ src/HW2/HW2.java
@@ -2,9 +2,21 @@ package HW2;
 
 public class HW2 {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("abba"));
+//        System.out.println(isPalindrome("abba"));
+//        System.out.println(isPalindrome("abwba"));
+//        System.out.println(isPalindrome("abbaaaa"));
+
+        long start = System.currentTimeMillis();
         System.out.println(isPalindrome("abwba"));
-        System.out.println(isPalindrome("abbaaaa"));
+        long finish = System.currentTimeMillis();
+        long elapsed = finish - start;
+        System.out.println("Прошло времени, мс: " + elapsed);
+
+        long start1 = System.currentTimeMillis();
+        System.out.println(isPalindrome2("abwba"));
+        long finish1 = System.currentTimeMillis();
+        long elapsed1 = finish1 - start1;
+        System.out.println("Прошло времени, мс: " + elapsed1);
     }
 
     static boolean isPalindrome(String word){
@@ -14,6 +26,18 @@ public class HW2 {
             rev.append(word.charAt(i));
         }
         if(rev.toString().equals(word)){
+            flag = true;
+        }
+        return flag;
+    }
+
+    static boolean isPalindrome2(String word){
+        String rev = "";
+        boolean flag = false;
+        for (int i = word.length()-1; i>=0; i--){
+            rev += word.charAt(i);
+        }
+        if(rev.equals(word)){
             flag = true;
         }
         return flag;
